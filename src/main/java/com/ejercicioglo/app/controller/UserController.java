c3410fc6ffefb8c7c74958dacbcc578473715f29
gloria <glorycaridad91@gmail.com>
2021-04-12 17:40:48 -0300

    comienzo del proyecto

---
+++ src/main/java/com/ejercicioglo/app/controller/UserController.java
@@ -0,0 +1,91 @@
+package com.ejercicioglo.app.controller;
+
+import com.ejercicioglo.app.entity.User;
+import com.ejercicioglo.app.service.UserService;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // Create a new user
+    @PostMapping
+    public ResponseEntity<?> create (@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
+
+    }
+
+    //Read an user
+    @GetMapping("/{id}")
+    public ResponseEntity<?> read(@PathVariable(value = "id") Long id){
+        Optional<User> optionalUser = userService.findById(id);
+
+        if (!optionalUser.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optionalUser);
+    }
+
+    //Update an user
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody User userDetails, @PathVariable(value = "id") Long userId){
+        Optional<User> user = userService.findById(userId);
+
+        if(!user.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        user.get().setName(userDetails.getName());
+        user.get().setSurname(userDetails.getSurname());
+        user.get().setEmail(userDetails.getEmail());
+        user.get().setEnabled(userDetails.isEnabled());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user.get()));
+    }
+
+    //Delete an user
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long userId){
+
+        if (!userService.findById(userId).isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        userService.deleteById(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    //Read all Users
+    @GetMapping
+    public List<User> readAll(){
+        List<User> users = (List<User>) StreamSupport
+                .stream(userService.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+
+        return users;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/ejercicioglo/app/entity/User.java
@@ -0,0 +1,72 @@
+package com.ejercicioglo.app.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    private String surname;
+
+    @Column(name = "mail", nullable = false, length = 50, unique = true)
+    private String email;
+
+    private boolean enabled;
+
+    public User() {
+    }
+
+    public User(String name, String surname, String email, boolean enabled) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.enabled = enabled;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
